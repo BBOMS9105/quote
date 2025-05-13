c1fd8a4744557781c09257c876ad72c1590fbcab
BBOMS9105 <bboms9105@gmail.com>
2025-05-13 12:44:07 +0900

    Initial commit

---
+++ src/quote/quote.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { QuoteService } from './quote.service';
+
+@Controller('quote')
+export class QuoteController {
+  constructor (private readonly quoteService: QuoteService) {}
+
+  @Get()
+  getQuote(): string {
+    return this.quoteService.getRandomQuote();
+  }
+}
